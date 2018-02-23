5a57a3f06f6d45bc5136c9173be2a5132a324e3d
Nicholas Virag <nickviragmusic@gmail.com>
2018-02-22 22:12:39 -0500

    Installed redux, generated initial markup

--- src/FetchInput.js
+++ src/FetchInput.js
@@ -1,10 +1,41 @@
 require('babel-polyfill');
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import CircularProgress from 'material-ui/CircularProgress';
+
+const style = {
+    margin: 12
+};
 
 export default class FetchInput extends Component {
     render() {
         return (
-            <div className="App">Hello</div>
+            <MuiThemeProvider>
+                <div className="App">
+                    Hello
+                </div>
+                <form
+                    onSubmit=
+                        { e => {
+                            e.preventDefault();
+                            this.props.fetchTopics(this.props.queryString);
+                        }}
+                    className="Input"
+                >
+                    <TextField
+                        hintText="Brooklyn, NY"
+                        floatingLabelText="Location"
+                        onChange=
+                            { e => {
+                                this.props.setQueryString(e.target.value);
+                            }}
+                        value={this.props.queryString}
+                    /><br />
+                    <RaisedButton type="submit" label="Primary" primary={true} style={style} />
+                </form>
+            </MuiThemeProvider>
         );
     }
 }
